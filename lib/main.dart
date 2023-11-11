49df87e68beff530eff8f29c4573233f2711d504
KongJen <supakorn.12.06.2003@gmail.com>
2023-11-12 04:24:15 +0700

    Add for can use this app in android.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './welcome/onboarding.dart';
+import './firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions
+    .currentPlatform,
+  );
   runApp(MyApp());
 }
 
